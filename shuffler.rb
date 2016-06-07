cd329b14b1a7be13f0c010cb54a6e460d415a64a
ks2355244 <ks2355244@yahoo.com>
2016-06-07 16:12:49 +0800

    shuffler edited

--- shuffler.rb
+++ shuffler.rb
@@ -1,13 +1,17 @@
 def shuffle(array)
   # Implement the shuffle method
-  array.rand == :sorted_array
+  array = Random.new
+  array.rand(10)
 end
 
+# .rand is not a default ruby method
+# .rand on an array or anything else
+# only this wrorks: rand(1000)
 
 # Driver code:
 sorted_array = (1..10).to_a
 
 # This should print a different sequence of numbers each time
 p shuffle(sorted_array)
-p shuffle(sorted_array)
-p shuffle(sorted_array)
+# p shuffle(sorted_array)
+# p shuffle(sorted_array)
